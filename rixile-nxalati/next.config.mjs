da9a093c21d80883e7b257a1ef1005548801b653
Josh <joshua.lekay@icloud.com>
2026-04-07 00:23:47 +0200

    Force build bypass

--- rixile-nxalati/next.config.mjs
+++ rixile-nxalati/next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This allows the build to finish even if there are type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // This allows the build to finish even if there are linting warnings
     ignoreDuringBuilds: true,
   },
 };
