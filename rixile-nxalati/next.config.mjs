abc97ffc1515a2a301849cac918fda46f21bfeb7
Josh <joshua.lekay@icloud.com>
2026-04-07 00:31:55 +0200

    Deleted extra config and saved bypass

--- rixile-nxalati/next.config.mjs
+++ rixile-nxalati/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This is the magic line that ignores the errors
     ignoreBuildErrors: true,
   },
   eslint: {
