cd584291e3a9b6fa5fe4ce3b9fa8d04531eb3ee1
Josh <joshua.lekay@icloud.com>
2026-04-07 00:29:35 +0200

    Actually saved the config this time

--- rixile-nxalati/next.config.mjs
+++ rixile-nxalati/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // This is the magic line that ignores the errors
     ignoreBuildErrors: true,
   },
   eslint: {
