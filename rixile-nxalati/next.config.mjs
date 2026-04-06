0a721d65d7b34ed256986111b76f0fef6b1c2323
Josh <joshua.lekay@icloud.com>
2026-04-07 00:52:47 +0200

    Fixed spelling and added all bypasses

--- rixile-nxalati/next.config.mjs
+++ rixile-nxalati/next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // This tells Vercel to ignore those 8 quote errors
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // This ignores the animation errors
     ignoreBuildErrors: true,
   },
 };
